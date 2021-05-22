1f8d2dcf57638fa554d125617616f727b11b0d4f
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-05-22 22:01:46 +0530

    Max Sum Contiguous Subarray
    https://www.interviewbit.com/problems/max-sum-contiguous-subarray/

---
+++ Max Sum Contiguous Subarray.cpp
@@ -0,0 +1,12 @@
+int Solution::maxSubArray(const vector<int> &nums) {
+    int curr_max=nums[0];
+    int max_sofar=nums[0];
+    int n=nums.size();
+    for(int i=1;i<n;i++)
+    {
+        curr_max=max(nums[i],curr_max+nums[i]);
+        max_sofar=max(max_sofar,curr_max);
+        
+    }
+    return max_sofar;
+}
