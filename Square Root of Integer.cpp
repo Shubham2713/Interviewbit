ffa8847c672dd2fb775c2a5102e04856038cfe4e
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-07-05 12:59:08 +0530

    Square Root of Integer
    https://www.interviewbit.com/problems/square-root-of-integer/

--- Square Root of Integer.cpp
+++ Square Root of Integer.cpp
@@ -1,20 +1,16 @@
-int Solution::sqrt(int x) {
-    if(x==0)
-    return x;
-    int low=1;
-    int high=x;
-    while(low<=high)
+int Solution::sqrt(int n) {
+    long long int lo=0;
+    long long int hi=n;
+    long long int mid;
+    while(lo<hi)
     {
-        long mid=low+(high-low)/2;
-        long sqr=mid*mid;
-        if(sqr==x)
-        return mid;
-        else if(sqr>x)
-        high=(int)mid-1;
+        mid=lo+(hi-lo+1)/2;
+
+        if(mid*mid>n)
+            hi=mid-1;
         else
-        low=(int)mid+1;
+        lo=mid;
         
     }
-    return high;
+    return lo;
 }
-
